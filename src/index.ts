d44ed8a06ebbc3b278c590bf77d76a6b70696680
GreatEki <ekeneidiagbor@gmail.com>
2023-04-03 16:47:26 +0100

    feat: nats impl for publishing and subcribing to events

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ export * from "./errors/index";
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
+
+export * from "./events/subjects";
+
+export * from "./events/listener/BaseListener";
+export * from "./events/publisher/BasePublisher";
+
+export * from "./events/listener/ticket-created-event";
+export * from "./events/listener/ticket-updated-event";
